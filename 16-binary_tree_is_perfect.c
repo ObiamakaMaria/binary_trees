0a172a4a0985eeb0e945e0865b24425db0304bb0
ObiamakaMaria <obiamaka0101@gmail.com>
2023-11-02 04:13:25 -0700

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,44 +1,83 @@
 #include "binary_trees.h"
 
+int recursive_full_check(const binary_tree_t *custom_tree);
+
 /**
- * binary_tree_height - This function calculates the
- * height of a binary tree
- * @tree: Thus is the pointer to the root node of the tree
- * Return: Returns the height of the tree else, 0 if tree is NULL
+ * binary_tree_is_perfect - Check if binary tree is perfect
+ * @custom_tree: Pointer to the node
+ * Return: 1 if perfect, otherwise 0
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *custom_tree)
 {
-	if (tree == NULL)
+	size_t a, b;
+
+	if (custom_tree == NULL)
+	{
 		return (0);
-
-	size_t l_height = binary_tree_height(tree->left);
-	size_t r_height = binary_tree_height(tree->right);
-
-	return (1 + (l_height > r_height ? l_height : r_height));
+	}
+	a = binary_tree_is_full(custom_tree) ? 1 : 0;
+	b = binary_tree_balance(custom_tree) == 0 ? 1 : 0;
+	return (a && b ? 1 : 0);
 }
 
 /**
- * binary_tree_is_perfect - This function checks if
- * a binary tree is perfect
- * @tree: This is the pointer to the root node of the tree
- * Return: Returns 1 if the tree is perfect OR 0 otherwise
+ * binary_tree_is_full - Check if binary tree is full
+ * @custom_tree: Pointer to the node
+ * Return: 1 if full otherwise 0
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *custom_tree)
 {
-	if (tree == NULL)
-		return (0);
-
-	size_t l_height = binary_tree_height(tree->left);
-	size_t r_height = binary_tree_height(tree->right);
-
-	if (l_height == r_height)
+	if (custom_tree == NULL)
 	{
-		if (tree->left && tree->right)
-			return (binary_tree_is_perfect(tree->left) &&
-					binary_tree_is_perfect(tree->right));
-		else if (tree->left == NULL && tree->right == NULL)
-			return (1);
+		return (0);
 	}
+	return (recursive_full_check(custom_tree));
+}
 
+/**
+ * recursive_full_check - Check if full recursively
+ * @custom_tree: Pointer to the node
+ * Return: 1 if full, 0 otherwise
+ */
+int recursive_full_check(const binary_tree_t *custom_tree)
+{
+	if (custom_tree != NULL)
+	{
+		if ((custom_tree->left != NULL && custom_tree->right == NULL) ||
+				(custom_tree->left == NULL && custom_tree->right != NULL) ||
+				recursive_full_check(custom_tree->left) == 0 ||
+				recursive_full_check(custom_tree->right) == 0)
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_balance - Calculate the tree balance
+ * @custom_tree: Pointer to the tree
+ * Return: Balance factor or 0 if NULL
+ */
+int binary_tree_balance(const binary_tree_t *custom_tree)
+{
+	if (custom_tree)
+	{
+		return (binary_tree_height(custom_tree->left) - binary_tree_height(custom_tree->right));
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_height - Calculate the height of a binary tree.
+ * @custom_tree: Pointer to the tree root node.
+ * Return: Height or 0 if tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *custom_tree)
+{
+	if (custom_tree)
+	{
+		size_t left_height = custom_tree->left ? 1 + binary_tree_height(custom_tree->left) : 1;
+		size_t right_height = custom_tree->right ? 1 + binary_tree_height(custom_tree->right) : 1;
+		return ((left_height > right_height) ? left_height : right_height);
+	}
 	return (0);
 }
